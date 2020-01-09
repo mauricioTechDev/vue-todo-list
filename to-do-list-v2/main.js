972c95ff41fa5e03294422231eae900724933c8a
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-01-08 23:07:30 -0500

    first commit

---
+++ to-do-list-v2/main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: "Vue to do list",
+    todoItem: "",
+    listOfTodos: []
+  },
+  methods: {
+    addToList() {
+      this.listOfTodos.push(this.todoItem)
+      this.todoItem = null
+    },
+
+  }
+})
