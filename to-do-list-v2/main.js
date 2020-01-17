c956f4191312f025ff4a52c2c6c8b7a07c46e116
resilient-new07 <mauricio.acosta92@gmail.com>
2020-01-17 12:28:58 -0500

    template

---
+++ to-do-list-v2/main.js
@@ -0,0 +1,53 @@
+Vue.component('todoList', {
+  template: `
+  <div class="todoList">
+  <h1>{{ title }}</h1>
+
+      <form @submit.prevent="addTodo">
+        <label for="newTodo">New Todo</label>
+        <input v-model="newTodo" type="text" name="newTodo" id="newTodo" value="">
+        <button type="submit" name="button">Add</button>
+      </form>
+
+      <!-- <h2>{{ newTodo }}</h2> -->
+
+      <ul>
+        <li v-for="item in itemsTodo">
+          <input type="checkbox" v-model="item.done">
+          <span :class="{ completedItem: item.done }">{{ item.title }}</span>
+          <span type="button" name="button" @click="removeTodo(item)">🗑️</span>
+        </li>
+      </ul>
+  </div>
+
+  `,
+  data() {
+    return{
+      title: "The Great Todo List",
+      newTodo: "",
+    }
+  },
+  methods: {
+    addTodo: function() {
+      this.itemsTodo.push({
+        title: this.newTodo,
+        done: false
+      })
+      this.newTodo = null
+    },
+    removeTodo(item){
+      const todoIndex = this.itemsTodo.indexOf(item);
+      this.itemsTodo.splice(todoIndex, 1);
+    }
+  }
+})
+
+
+
+var app = new Vue({
+  el: '#app',
+  data:{
+    itemsTodo: []
+  }
+
+})
